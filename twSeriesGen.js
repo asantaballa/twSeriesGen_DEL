3894e547fbdab78cf764f2bbda789d1d0cb956ef
ALBERTO SANTABALLA <albertosantaballa@ALBERTOs-MacBook-Pro.local>
2016-03-25 05:29:28 -0700

    Add some basic structure

--- twSeriesGen.js
+++ twSeriesGen.js
@@ -2,3 +2,35 @@ function GenTestMsg()
 {
 	alert('GenTestMsg: Better message');
 }
+
+//-- Class SeriesGen -- //
+
+function SeriesGen()
+{
+	this._Series = [];
+}
+
+SeriesGen.prototype.Gen = function()
+{
+	this.AddPoint("Item 01");
+	this.AddPoint("Item 02");
+	this.AddPoint("Item 03");
+}
+
+SeriesGen.prototype.AddPoint = function(value)
+{
+	this._Series.push(new SeriesGenPoint(value));
+}
+
+SeriesGen.prototype.getSeries = function()
+{
+	return this._Series;
+}
+
+// -- Class SeriesGenPoint -- //
+
+function SeriesGenPoint(value)
+{
+	this.Value = value;
+	return this;
+}
